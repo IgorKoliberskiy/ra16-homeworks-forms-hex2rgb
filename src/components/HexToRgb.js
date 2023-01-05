b6240d54372cf933c546d8896964babbd4013a66
Игорь Колиберский <kolibry60rus@mail.ru>
2023-01-05 22:02:14 +0300

    HW forms-hex2rgb

---
+++ src/components/HexToRgb.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  return result ? `rgb(${[1, 2, 3].map((o) => parseInt(result[o], 16)).join(', ')})` : false;
+}
+
+export default function HexToRgb() {
+  const [hex, setHex] = useState('')
+  const [rgb, setRgb] = useState('')
+  
+  const handleSetColor = (evt) => {
+    const { value } = evt.target;
+
+    setHex(value);
+
+    const app = document.querySelector('.App');
+
+    if (value.length === 7 && value[0] === '#') {
+      setRgb(hexToRgb(value))
+      app.style.backgroundColor = value
+    } 
+  }
+  
+  return (
+    <form>
+      <input
+        className='hex'
+        type='text' 
+        value={hex} 
+        onChange={handleSetColor} 
+        maxLength={7} 
+      />
+      <br />
+      <input 
+        className='rgb'
+        type='text'
+        value={rgb ? rgb : 'Ошибка!'} 
+        disabled 
+      />
+    </form>
+  )
+} 
